3ab4949ebe42c84f92a763a90da70db293ab48ae
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-10-26 01:08:23 +0300

    still weather to find date

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # city_explorer_api
+# Project Name
+
+**Author**: hadeel hhawajreh
+**Version**: 1.0.0 
+
+## Overview
+learning how to use server
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+.env => (init PORT =NUM )
+
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+languages :json,js
+libraries: .env,npm, node,express 
+need server.js and deploy the work heroku by github
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
+
+## Credits and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+-->
--- server.js
+++ server.js
@@ -4,23 +4,36 @@ let cors=require('cors');
 
 let app=express();
 app.use(cors());//allow all devices acess to my server 
-
 require('dotenv').config();
-app.get('/location',hadleLocation );//calling function withut ()
-function hadleLocation(req,res){// req,res are var
 
+app.get('/location',hadleLocation );//calling function withut ()
+
+function hadleLocation(req,res){// req,res are var
+try{
 let city=req.query.city;//query par from city يعني من اللينك وين بدي اروح :()/amman or /seattle 
 let jsonData=require('./data/location.json');
 let jsonOjb=jsonData[0];
 let locationObjCons=new Location(city,jsonOjb.d,jsonOjb.display_name,jsonOjb.let,jsonOjb.lon);//from file name is location.json
 res.status(200).json(locationObjCons);//converting to json and sent it 
 }
+}
+catch(error){
+    res.status(500).send('Sorry, something went wrong');
+}
 
+// location constr
 function Location(search_query,formatted_query,latitude,longitude){
 this.search_query=search_query;
 this.formatted_query=formatted_query;
 this.latitude=latitude;
 this.longitude=longitude;
+}
+
+// Weather constr
+
+function Weather(forecast,time){
+    this.forecast=forecast;
+    this.time=time;
 }
 // {
 //     "search_query": "seattle",
@@ -32,3 +45,31 @@ const PORT=process.env.PORT;
 app.listen(PORT, ()=>{
     console.log(`app is listening  to  ${PORT}`);
 })
+
+app.get('/weather',hadleWeather );//calling function withut ()
+function hadleWeather(req,res){// req,res are var
+    try{
+    let weather=req.query.city;//query par from city يعني من اللينك وين بدي اروح :()/amman or /seattle 
+    let jsonData=require('./data/weather.json');
+    let jsonOjb=jsonData.data;
+    jsonOjb.forEach(element => {
+        
+    });
+    let weatherObjCons=new Weather(city,jsonOjb.data.weather.description,jsonOjb.display_name,);//from file name is location.json
+    res.status(200).json(locationObjCons);//converting to json and sent it 
+    }
+    }
+    catch(error){
+        res.status(500).send('Sorry, something went wrong');
+    }
+// [
+//     {
+//       "forecast": "Partly cloudy until afternoon.",
+//       "time": "Mon Jan 01 2001"
+//     },
+//     {
+//       "forecast": "Mostly cloudy in the morning.",
+//       "time": "Tue Jan 02 2001"
+//     },
+//     ...
+//   ]
