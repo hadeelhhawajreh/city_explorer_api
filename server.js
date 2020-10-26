fa1b5e4b93ed2e05fbc6e1d69014bee98b1f2b22
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-10-26 16:49:45 +0300

    another server

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 // should e in every sever ...---save it
-let express = require('express');
-let cors = require('cors');
-
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
 let app = express();
 app.use(cors());//allow all devices acess to my server 
-require('dotenv').config();
-
 app.get('/location', hadleLocation);//calling function withut ()
 const PORT = process.env.PORT;
 
 function hadleLocation(request, response) {// req,res are var
-    try {
-        let city = request.query.city;//query par from city يعني من اللينك وين بدي اروح :()/amman or /seattle 
-        let jsonData = require('./data/location.json');//file 
-        let jsonOjb = jsonData[0];
-        let locationObjCons = new Location(city, jsonOjb.display_name, jsonOjb.lat, jsonOjb.lon);//from file name is location.json
-        response.status(200).json(locationObjCons);//converting to json and sent it 
-  
+    let key = process.env.GEOCODE_API_KEY;
+    let city = request.query.city;//query par from city يعني من اللينك وين بدي اروح :()/amman or /seattle 
+    superagent.get(`http://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`)
+        .then((data) => {
+            let jsonOjb = data.body[0];
+            console.log(jsonOjb);
+            // let jsonData = require('./data/location.json');//file 
+            let locationObjCons = new Location(city, jsonOjb.display_name, jsonOjb.lat, jsonOjb.lon);//from file name is location.json
+            response.status(200).json(locationObjCons);//converting to json and sent it 
+
+        }).catch(()=>{
+            response.status(200).send('somrthing error ...');
+        });
+//
 
 
-}catch (error) {
-    response.status(500).send('Sorry, something went wrong');
-}}
+
+}
 
 // location constr
 function Location(search_query, formatted_query, latitude, longitude) {
@@ -30,8 +35,6 @@ function Location(search_query, formatted_query, latitude, longitude) {
     this.latitude = latitude;
     this.longitude = longitude;
 }
-
-
 
 app.listen(PORT, () => {
     console.log(`app is listening  to  ${PORT}`);
@@ -68,29 +71,29 @@ app.listen(PORT, () => {
 
 
 // //solving wether 
-function Weather(forecast, time) {
-    this.forecast = forecast;
-    this.time = time;
-}
+// function Weather(forecast, time) {
+//     this.forecast = forecast;
+//     this.time = time;
+// }
 
-app.get('/weather', hadleWeather);//calling function withut ()
-function hadleWeather(req, res) {// req,res are var
-    try {
-        let weather = req.query.city;//query par from city يعني من اللينك وين بدي اروح :()/amman or /seattle 
-        let jsonData = require('./data/weather.json');
-        let jsonOjbW = jsonData.data;
-        let weatherArray=[];
-        jsonOjbW.forEach(element => {
-            let toDate= element.datetime;
-            let temp=new Date(toDate).split('');
-            temp=new Date(temp[0],temp[1],temp[2])
-            let weatherObjCons = new Weather( element.weather.description, temp);//from file name is weather.json
-            weatherArray.push(weatherObjCons);
-        });
-        res.status(200).json(weatherArray);//converting to json and sent it 
-    }
-    
-    catch (error) {
-    res.status(500).send('Sorry, something went wrong');
-}
-}
+// app.get('/weather', hadleWeather);//calling function withut ()
+// function hadleWeather(req, res) {// req,res are var
+//     try {
+//         let weather = req.query.city;//query par from city يعني من اللينك وين بدي اروح :()/amman or /seattle 
+//         let jsonData = require('./data/weather.json');
+//         let jsonOjbW = jsonData.data;
+//         let weatherArray = [];
+//         jsonOjbW.forEach(element => {
+//             let toDate = element.datetime;
+//             let temp = new Date(toDate).split('');
+//             temp = new Date(temp[0], temp[1], temp[2]);
+//             let weatherObjCons = new Weather(element.weather.description, temp);//from file name is weather.json
+//             weatherArray.push(weatherObjCons);
+//         });
+//         res.status(200).json(weatherArray);//converting to json and sent it 
+//     }
+
+//     catch (error) {
+//         res.status(500).send('Sorry, something went wrong');
+//     }
+// }
