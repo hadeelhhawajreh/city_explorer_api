e0c6b75efab6f6d253267bce0502675e37f2a8f7
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-10-26 11:04:28 +0300

    adding weather

--- server.js
+++ server.js
@@ -68,28 +68,29 @@ app.listen(PORT, () => {
 
 
 // //solving wether 
-// function Weather(forecast, time) {
-//     this.forecast = forecast;
-//     this.time = time;
-// }
+function Weather(forecast, time) {
+    this.forecast = forecast;
+    this.time = time;
+}
 
-// app.get('/weather', hadleWeather);//calling function withut ()
-// function hadleWeather(req, res) {// req,res are var
-//     try {
-//         let weather = req.query.city;//query par from city يعني من اللينك وين بدي اروح :()/amman or /seattle 
-//         let jsonData = require('./data/weather.json');
-//         let jsonOjbW = jsonData.data;
-//         let weatherArray=[];
-//         jsonOjbW.forEach(element => {
-//             let toDate= element.datetime;
-//             let temp=new Date(toDate).split('');
-//             temp=new Date(temp[0],temp[1],temp[2])
-//             let weatherObjCons = new Weather( element.weather.description, temp);//from file name is weather.json
-//             weatherArray.push(weatherObjCons);
-//         });
-//         res.status(200).json(weatherObjCons);//converting to json and sent it 
-//     }
-//     }
-//     catch (error) {
-//     res.status(500).send('Sorry, something went wrong');
-// }
+app.get('/weather', hadleWeather);//calling function withut ()
+function hadleWeather(req, res) {// req,res are var
+    try {
+        let weather = req.query.city;//query par from city يعني من اللينك وين بدي اروح :()/amman or /seattle 
+        let jsonData = require('./data/weather.json');
+        let jsonOjbW = jsonData.data;
+        let weatherArray=[];
+        jsonOjbW.forEach(element => {
+            let toDate= element.datetime;
+            let temp=new Date(toDate).split('');
+            temp=new Date(temp[0],temp[1],temp[2])
+            let weatherObjCons = new Weather( element.weather.description, temp);//from file name is weather.json
+            weatherArray.push(weatherObjCons);
+        });
+        res.status(200).json(weatherObjCons);//converting to json and sent it 
+    }
+    
+    catch (error) {
+    res.status(500).send('Sorry, something went wrong');
+}
+}
